9703b87e629b6815835261aabe267a48768a6d3d
BlockPanther <47432639+BlockPanther@users.noreply.github.com>
2019-02-26 21:44:40 +0000

    Fix Statisticspage for Ubuntu Build
    Fix Statisticspage for Building Block Panther GUI Wallet on Ubuntu

--- src/qt/statisticspage.cpp
+++ src/qt/statisticspage.cpp
@@ -120,7 +120,7 @@ void StatisticsPage::updateStatistics()
     QString Qlpawrate = model->getLastBlockDate().toString();
 
     QString QPeers = QString::number(peers);
-    QString qVolume = QLocale(QLocale::English).toString(volume);
+    QString qVolume = QLocale(QLocale::English).toString((qlonglong)volume);
 
     if(nHeight > heightPrevious)
     {
