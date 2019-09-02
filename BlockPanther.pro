9ad2cb8cf184a8894cd210604aabbbef184bc760
BlockPanther <47432639+BlockPanther@users.noreply.github.com>
2019-09-02 12:10:11 +0100

    Client Update v1.1
    BlockPanther Client update v1.1. Please ensure you update.

--- BlockPanther.pro
+++ BlockPanther.pro
@@ -1,6 +1,6 @@
 TEMPLATE = app
 TARGET = Block-Panther
-VERSION = 1.0
+VERSION = 1.1
 INCLUDEPATH += src src/json src/qt
 QT += core gui network
 DEFINES += QT_GUI BOOST_THREAD_USE_LIB BOOST_SPIRIT_THREADSAFE
--- src/version.h
+++ src/version.h
@@ -30,21 +30,21 @@ static const int DATABASE_VERSION = 80509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 80030;
+static const int PROTOCOL_VERSION = 80042;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 static const int MIN_PROTO_VERSION = 209;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
-static const int CADDR_TIME_VERSION = 31402;
+static const int CADDR_TIME_VERSION = 31405;
 
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 0;
-static const int NOBLKS_VERSION_END = 80008;
+static const int NOBLKS_VERSION_END = 80012;
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
-static const int BIP0031_VERSION = 80000;
+static const int BIP0031_VERSION = 80003;
 
 // "mempool" command, enhanced "getdata" behavior starts with this version:
 static const int MEMPOOL_GD_VERSION = 80002;
